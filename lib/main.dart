3ea46b7ff166f7d11310b8910f475bf97d94ae4f
Khilan Vitthani <106797515+KhilanVitthani@users.noreply.github.com>
2023-06-30 15:26:07 +0530

    change name logo and remove hase from url

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,26 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-
 import 'package:get/get.dart';
+import 'package:url_strategy/url_strategy.dart';
 
 import 'app/routes/app_pages.dart';
 import 'firebase_options.dart';
+
 initFireBaseApp() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 }
-void main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-await  initFireBaseApp();
 
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initFireBaseApp();
+  setPathUrlStrategy();
   runApp(
     GetMaterialApp(
-debugShowCheckedModeBanner: false,
-      title: "Application",
+      debugShowCheckedModeBanner: false,
+      title: "Buddha Quotes Admin",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
     ),
